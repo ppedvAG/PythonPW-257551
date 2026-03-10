a878524ee79d7580e1b0c74e8527ade40597c2ee
ppedvLukasK <lukask@ppedv.de>
2026-03-10 11:20:52 +0100

    M013

---
+++ M013.py
@@ -0,0 +1,49 @@
+# Lambda
+# Anonyme Funktionen
+# D.h., Funktionen die nicht per def definiert werden, sondern einmalig in eine Variable geschrieben werden
+# Dadurch sind diese Funktionen nicht wiederverwendbar, und werden nach Verwendung weggeworfen
+def halloWelt():
+	print("Hallo Welt")
+
+halloWelt()  # Diese Funktion kann jetzt überall verwendet werden
+
+x = halloWelt  # Hier wird jetzt ein Funktionszeiger gespeichert
+print(x)  # Über x kann jetzt auf die Funktion zugegriffen werden, ohne den originalen Namen zu kennen
+
+x()  # halloWelt() ausführen
+
+l = lambda: print("Hallo Welt")  # Funktion direkt in Variable legen ohne def zu Verwenden
+l()
+
+###############################################
+
+# filter, map
+
+# filter
+# Filtert eine Liste anhand einer Bedingung
+zahlen = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+durch2 = []
+for x in zahlen:
+	if x % 2 == 0:
+		durch2.append(x)
+
+def teilbarDurch2(x: int):
+	return x % 2 == 0
+
+# Hier wird eine Schleife im Hintergrund ausgeführt
+# Bei jedem Element wird die Funktion ausgeführt, wenn diese True zurückgibt, kommt die jetztige Zahl in die Ergebnismenge hinein
+y = filter(teilbarDurch2, zahlen)
+print(list(y))
+
+z = filter(lambda x: x % 2 == 0, zahlen)
+print(list(z))
+
+# map
+# Transformiert jedes Element der Liste in eine neue Form
+
+# Aufgabe: Jede Zahl ** 2
+# Hier wird eine Schleife im Hintergrund ausgeführt
+# Bei jedem Element wird die Funktion ausgeführt, die jetztige Zahl kommt in ihrer neuen Form in die Ergebnismenge hinein
+h = map(lambda x: x ** 2, zahlen)  # Neue Form: x ** 2
+print(list(h))
